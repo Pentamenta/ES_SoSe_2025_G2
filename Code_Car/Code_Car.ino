5c4d2d2d4f9a6f545fb55b3a4464eb5505e92e7c
Adrian Behrens <adbe6030@bht-berlin.de>
2025-04-12 15:02:31 +0200

    Basic BLE both programms

--- Code_Car/Code_Car.ino
+++ Code_Car/Code_Car.ino
@@ -1,8 +1,11 @@
-#include "EBS_BLE.h"
+#include <ArduinoBLE.h>
+#include "EBS_BLE.h" //Custom Header mit BLE definitionen (Adrian)
 
 void setup() {
+Serial.begin(9600);
 
-a = 1;
+BLE_Setup(); //Öffnet die BLE-Schnittstelle und initiallisiert das Peripherial Device (Adrian)
+
 
 }
 
@@ -10,4 +13,27 @@ void loop() {
 
 
 
+}
+
+void BLE_Setup(){ //Öffnet die BLE-Schnittstelle und initiallisiert das Peripherial Device (Adrian)
+// Ist unbedingt erst nach Serial.begin() auszuführen.
+
+  if (!BLE.begin()) { //Funktion des BLE Moduls überprüfen
+    Serial.println("* Starting Bluetooth® Low Energy module failed!");
+    while (1);
+  }
+  
+  BLE.setLocalName("Car (Peripherial)"); //Namen des Central Devices Festlegen
+
+  BLE.setAdvertisedService(remote_service); //Setzt die UUID des Services, wenn er advertised wird
+  remote_service.addCharacteristic(speed_target); //Charakteristiken zum Service hinzufügen
+  
+  BLE.addService(remote_service); //Fügt den Service zu der Liste an verfügbaren Services hinzu.
+  
+  speed_target.writeValue(speed_target);
+  BLE.advertise();
+
+  Serial.println("Car (Dashboard Device)");
+  Serial.println(" ");
+  
 }
--- Code_Dashboard/EBS_BLE.h
+++ Code_Dashboard/EBS_BLE.h
@@ -3,10 +3,10 @@
 
 ///BLE Variablen (Adrian)
 
-int speed_target = 0;
-int speed_actual = 0;
+int speed_target_val = 100;
+int speed_actual_val = 100;
 
-///UUID Dekleration (Adrian)
+///UUID und BLE Dekleration (Adrian)
 
 //UUID des vom Dashboard bereitgestellten Services
 const char* remote_service_Uuid = "9aeb113b-c580-4bd1-8eb2-deefe0124d0d"; 
@@ -14,5 +14,9 @@ const char* remote_service_Uuid = "9aeb113b-c580-4bd1-8eb2-deefe0124d0d";
 //UUID des vom Dashboard bereitgestellten Services
 const char* speed_target_Uuid = "014b35b7-5ae7-44e8-810b-95d17f2b9170"; //Soll-Geschwindigkeit, die ans Fahrzeug gesendet wird
 const char* speed_actual_Uuid = "a56a5afb-44ad-4a5b-9f31-36e0aa80f513"; //Ist-Geschwindigkeit, die das Fahrzeug zurück gibt
+
+
+BLEService remote_service(remote_service_Uuid);
+BLEIntCharacteristic speed_target(speed_target_Uuid, BLERead | BLEWrite);
 
 #endif

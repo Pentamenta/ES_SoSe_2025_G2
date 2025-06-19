74cea3f78a1371f09b56ea34acf8f6e21aa05426
Adrian Behrens <adbe6030@bht-berlin.de>
2025-06-19 13:05:29 +0200

    added stearing

--- Code_Dashboard/Code_Dashboard.ino
+++ Code_Dashboard/Code_Dashboard.ino
@@ -2,6 +2,7 @@
 
 #define BLE_LED 4
 #define MAX_SPEED 10 //m/s
+#define MAX_STEAR 10 // Ausschlag zum Steuern
 
 #include <ArduinoBLE.h> 
 #include "EBS_BLE.h"  //Custom Header mit BLE definitionen (Adrian)
@@ -37,6 +38,7 @@ void loop() {
   }
 
   if (millis() >= t_fast + FAST_CYCLE) { // schnelle BLE und Serial Kommunikation (Adrian)
+    data_to_car.stear_target_val = read_stear();
     data_to_car.speed_target_val = read_speed();
     BLE_fast_exchange();
     t_fast = millis();
@@ -56,7 +58,7 @@ void loop() {
 
 }
 
-float read_speed() {
+float read_speed() { // Ließt linken Joystick für Beschleunigung aus (Adrian)
 
 int buffer = read_joystick_pos(1);
 float max = MAX_SPEED;
@@ -64,5 +66,16 @@ float dead = DEADZONE_LOWER;
 
 float speed = (max/dead)*buffer;
 return speed;
+
+}
+
+float read_stear() { // Ließt rechten Joystick für Lenkung aus (Adrian)
+
+int buffer = read_joystick_pos(2);
+float max = MAX_STEAR;
+float dead = DEADZONE_LOWER;
+
+float stear = (max/dead)*buffer;
+return stear;
 
 }
